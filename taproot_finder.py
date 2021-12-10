2792e4566bff5ab27bc8e86dcd0b04b3d4e7699a
ArTombado <45319004+ArTombado@users.noreply.github.com>
2021-12-10 15:15:42 -0300

    Update taproot_finder.py

--- taproot_finder.py
+++ taproot_finder.py
@@ -10,13 +10,13 @@ block_hashes = getBlockRange(from_block, to_block)
 taproot = []
 
 for block_num in block_hashes:
-    txs = asyncio.run(getBlock(block_hashes[block_num]))
+	txs = asyncio.run(getBlock(block_hashes[block_num]))
 
-    print(f"Checking block {block_num}.")
-
-    for tx in txs:
-        if( hasTaprootAddress(tx) ):
-            taproot.append(tx)
+	print(f"Checking block {block_num}.")
+	
+	for tx in txs:
+		if( hasTaprootAddress(tx) ):
+			taproot.append(tx)
 
 for tap in taproot:
 	print(tap["txid"])
